148b2fd398ca1e67fc15ecb3ad1db679990e05ff
Lewis <lewischarlesbaker@gmail.com>
2021-03-22 21:07:19 +0000

    working test with iteration

---
+++ lkml/test.py
@@ -0,0 +1,70 @@
+import lkml as looker
+from pprint import pprint
+
+import pandas as pd
+import os
+import pandas
+
+sql = """
+
+with source as (
+
+select *,
+
+case when data_type in  ('INT64','FLOAT64','NUMERIC') then ' number'
+when data_type = 'BOOL' then ' yesno'
+when data_type = 'TIMESTAMP' then ' time'
+when data_type =  'STRING' then ' string'
+
+end as looker_data_type
+
+from `project-x-ray.analytics_qa.INFORMATION_SCHEMA.COLUMNS`
+
+),
+
+facts as (
+select 
+*
+from source
+
+where table_name like '%fct%'
+
+or table_name like '%dim%'
+
+)
+
+select * from facts
+
+"""
+
+# Run a Standard SQL query using the environment's default project
+df = pandas.read_gbq(sql, dialect='standard')
+
+# Run a Standard SQL query with the project set explicitly
+project_id = 'ra-development'
+df = pandas.read_gbq(sql, project_id=project_id, dialect='standard')
+
+for row in df.itertuples():
+
+    view_name = {
+    
+    "includes": [row.table_name + *.view]
+
+    }
+
+    lookml = {
+
+        "includes": ["*.view"],
+
+        "dimension": [
+            {
+                "label": "Orders, Items and Users",
+                "type": row.data_type,
+                "sql_table_name": row.column_name,
+                        "name": row.column_name,
+                    }
+                ],
+
+            }
+
+    print(looker.dump(lookml))
